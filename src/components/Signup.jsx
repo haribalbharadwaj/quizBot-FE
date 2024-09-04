729a2b4b6ee2fd2a9171d1f8eb462a72a5088c21
Bharadwaj <haribalbharadwaj@gmail.com>
2024-09-03 19:36:25 -0700

    '2'

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -119,7 +119,7 @@ function Signup() {
         flexDirection:'column'
       }}>
 
-<div style={{width:'187px',height:'100px',top:'41px',left:'600px',position:'absolute'}}>
+<div style={{width:'187px',height:'100px',top:'136px',left:'625px',position:'absolute'}}>
             <span style={{fontFamily: 'Jomhuria,sans-serif',fontSize: '100px',fontWeight:'400',lineHeight:'100px',textAlign:'left',
               color:'#474444'
             }}>
