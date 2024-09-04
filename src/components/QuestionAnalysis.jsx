9318889b4219042d57a62c6d27eb7b17716e2a80
Bharadwaj <haribalbharadwaj@gmail.com>
2024-09-04 01:19:27 -0700

    '47'

--- src/components/QuestionAnalysis.jsx
+++ src/components/QuestionAnalysis.jsx
@@ -517,7 +517,7 @@ const handleCircleClick = (index) => {
 
       <div className="questions">
         {quiz.questions.map((question, qIndex) => (
-          <div key={question._id} className="question-section" style={{width:'828px',height:'159px',top:'120px',left:'88px',position:'relative',display:'flex',flexDirection:'column',
+          <div key={question._id} className="question-section" style={{width:'828px',height:'159px',top:'100px',left:'88px',position:'relative',display:'flex',flexDirection:'column',
             marginTop:'80px'
            }}>
             <h2 style={{fontFamily: 'Poppins, sans-serif', fontSize: '30px', fontWeight: '500', lineHeight: '45px', textAlign: 'left',}}>Question {qIndex + 1}: {question.questionText}</h2>
