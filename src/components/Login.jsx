e7cb680d5af10b29bd72c6520c3b26dc793c7ac5
Bharadwaj <haribalbharadwaj@gmail.com>
2024-09-03 20:14:31 -0700

    '23'

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -139,7 +139,7 @@ const handleRegister=()=>{
             </span>
           </div>
           <div style={{width: '700px',height:'37px',top:'250px',left:'32%',display:'flex',flexDirection:'row',position:'absolute',
-            gap:'130px'
+            gap:'100px'
           }}>
             <button style={{width:'217px',height:'37px',top:'267px',left:'422px',borderRadius:'10px',background:'#FFFFFF',
              color:'#474444',fontFamily:'Poppins,sans-serif',borderColor:'#FFFFFF',
